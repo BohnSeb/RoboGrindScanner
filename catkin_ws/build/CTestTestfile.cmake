cb27fb8f6283b3446865f7b8d134e08cdde2a783
Sebastian <seemax.bohn@googlemail.com>
2022-04-25 15:54:54 +0200

    deleted automatic generated folders

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Projects/RoboGrindScanner/catkin_ws/src
-# Build directory: E:/Projects/RoboGrindScanner/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("artec")
